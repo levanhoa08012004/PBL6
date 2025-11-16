6266ae4a47d4f30cf8ae85f191f9227520cce63c
hoa-123-afk <hoalv1237890@gmail.com>
2025-11-16 16:54:05 +0700

    deloy vercel

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -115,9 +115,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # middleware khác
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
