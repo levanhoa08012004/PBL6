d0a64c5400f8df3e21edc7e5bd49c0c3d09377db
hoa-123-afk <hoalv1237890@gmail.com>
2025-12-01 20:30:33 +0700

    fix render build

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -73,10 +73,10 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
 
